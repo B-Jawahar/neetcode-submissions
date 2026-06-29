d54f320a6fea213ea3ff50cee879d8997ed9cd03
Jawahar B <78546888+B-Jawahar@users.noreply.github.com>
2026-06-29 20:52:40 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lifo=[]
+        if len(s)==0:
+            return True
+        for i in s:
+            if lifo and lifo[-1]==i:
+                lifo.pop()
+            else:
+                if i=="{":
+                    lifo.append("}")
+                elif i=="[":
+                    lifo.append("]")
+                elif i=="(":
+                    lifo.append(")")
+                else:
+                    return False
+        if len(lifo)>0:
+            return False
+        else:
+            return True
+                
